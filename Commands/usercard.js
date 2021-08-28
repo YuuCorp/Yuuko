0ca7312b0f3d71272a76b6d64df4b2ecab61dd50
Sayykii <martin40lmg@gmail.com>
2021-08-28 12:29:30 +0300

    Cleaned up and added some comments to the usercard command. We need a new design.

--- Commands/usercard.js
+++ Commands/usercard.js
@@ -10,7 +10,7 @@ const Discord = require("discord.js"),
 
 module.exports = new Command({
     name: "usercard",
-    description: "Searches for an AniList user and displays a banner for it.",
+    description: "Searches for an AniList user and displays a banner with the user's manga and anime statistics",
     type: CommandCategories.AniList,
 
     async run(message, args, run) {
@@ -59,13 +59,15 @@ module.exports = new Command({
                     //    .setColor('0x00ff00')
                     //    .setFooter(Footer(response))
 
+                    //^ Create canvas
                     Canvas.registerFont(path.join(__dirname, "../Assets/OpenSans-SemiBold.ttf"), { family: "Open_Sans" });
                     const canvas = Canvas.createCanvas(1400, 330);
                     const ctx = canvas.getContext("2d");
                     const bg = await Canvas.loadImage("https://cdn.discordapp.com/attachments/875693863484932106/877230763261710397/anisuggest.png");
                     const pfp = await Canvas.loadImage(data.avatar.large);
 
-                    ctx.filter = "blur(4px)";
+                    //^ Show the Profile Picture and Background images
+                    //TODO Maybe people could select a custom background
                     ctx.drawImage(bg, 0, 0);
                     ctx.filter = "none";
                     ctx.drawImage(pfp, 50, 50);
@@ -73,24 +75,53 @@ module.exports = new Command({
                     ctx.textAlign = "center";
                     ctx.textBaseline = "top";
 
-                    const text = data.name.toUpperCase();
-                    const textWidth = ctx.measureText(text).width;
+                    //^ Get all of the text needed and measure its width
+                    //^ Profile name
+                    const name = data.name.toUpperCase();
+                    const nameWidth = ctx.measureText(name).width;
+
+                    //^ Anime statistics
+                    const anime = 'Anime';
                     const watched = `Watched: ${data.statistics.anime.count.toString()}`;
                     const watchedWidth = ctx.measureText(watched).width;
+                    const aMeanScore = `Score: ${data.statistics.anime.meanScore.toString()}%`;
 
-                    ctx.fillStyle = "rgba(255, 255, 255, 0.3);";
+                    //^ Manga statistics
+                    const manga = 'Manga';
+                    const read = `Completed: ${data.statistics.manga.count.toString()}`;
+                    //* Not needed right now
+                    const readWidth = ctx.measureText(read).width;
+                    const mMeanScore = `Score: ${data.statistics.manga.meanScore.toString()}%`;
 
-                    roundRect(ctx, canvas.width / 2 - watchedWidth / 2, textWidth, 120, 10, true, false);
-                    roundRect(ctx, canvas.width / 2 - watchedWidth / 2, 100, watchedWidth, 120, 10, true, false);
-                    //ctx.fillRect((canvas.width/2) - (textWidth / 2) - 15, 300, textWidth + 30, 120);
+                    //^ Create a round rectangle 
+                    ctx.fillStyle = "rgba(255, 255, 255, 0.2);";
+                    //roundRect(ctx, 800, 5, 600, 325, 100, true, false);
+                    //roundRect(ctx, canvas.width / 2 - watchedWidth / 2, 100, watchedWidth, 120, 10, true, false);
+                    ctx.fillRect(750, 5, 700, 320);
 
+                    //^ Display all of the text 
+                    
+                    //^ Name text
                     ctx.fillStyle = "#fff";
-                    ctx.font = "45pt Open_Sans";
+                    ctx.font = "35pt Open_Sans";
                     ctx.textAlign = "left";
-                    ctx.fillText(text, 290, 130);
+                    ctx.fillText(name, 290, 130);
+
+                    //^ Anime Completed/Mean Score text
                     ctx.textAlign = "center";
-                    ctx.font = "40pt Open_Sans";
-                    ctx.fillText(watched, 1000, canvas.height / 2);
+                    ctx.font = "30pt Open_Sans";
+                    ctx.fillText(anime, 1100, 20)
+                    ctx.font = "25pt Open_Sans";
+                    ctx.fillText(watched, 950, 90);
+                    ctx.fillText(aMeanScore, 1250, 90);
+
+                    //^ Manga Completed/Mean Score text
+                    ctx.textAlign = "center"
+                    ctx.font = "30pt Open_Sans";
+                    ctx.fillText(manga, 1100, 170);
+                    ctx.font = "25pt Open_Sans";
+                    ctx.fillText(read, 950, 250);
+                    ctx.fillText(mMeanScore, 1250, 250)
 
                     const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "anilist_banner.png");
                     message.channel.send({ files: [attachment] });
@@ -100,7 +131,7 @@ module.exports = new Command({
                 }
             })
             .catch((error) => {
-                // log axios request status code and error
+                //^ log axios request status code and error
                 if (error.response) {
                     console.log(error.response.data.errors);
                 } else {
