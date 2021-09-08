95696e833e2581d6747c1fd4fd755550ead23901
Tibix <fuloptibi03@gmail.com>
2021-09-09 00:29:58 +0300

    - Patched a security vulnerability where the bot could be tricked spamming commands to itself - Invalid command message formatting improvements

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -2,13 +2,14 @@ const Event = require("../Structures/Event.js");
 const config = require("../config.json");
 
 module.exports = new Event("messageCreate", (client, message) => {
-    if (!message.content.startsWith(client.prefix)) return;
+    // If it's not the prefix, or the author is the bot itself then reject
+    if (!message.content.startsWith(client.prefix) || message.author.bot) return;
     
     const args = message.content.substring(config.prefix.length).split(/ +/);
 
     const command = client.commands.find(cmd => cmd.name == args[0]);
 
-    if(!command) return message.reply(`${args[0]} is not a valid command.`);
+    if(!command) return message.reply(`\`${args[0]}\` is not a valid command.`);
 
     command.run(message, args, client);
 })
