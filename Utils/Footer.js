6230aa6783bec2c161d71106fbc82d80d05baeca
Fülöp Tibor <fuloptibi03@gmail.com>
2022-03-02 23:03:33 +0200

    Footer embedding deprication fix

--- Utils/Footer.js
+++ Utils/Footer.js
@@ -4,7 +4,8 @@
  * @returns {String} The footer string.
  */
 module.exports = (response = null) => {
-    return response
-        ? `AniSuggest v0.1 Beta (${response.headers["x-ratelimit-remaining"] + "/" + response.headers["x-ratelimit-limit"]}) | ${new Date().getHours() + ':' + new Date().getMinutes()}` 
-        : `AniSuggest v0.1 Beta | ${new Date().getHours() + ':' + new Date().getMinutes()}`
+    const footerString = response
+        ? `AniSuggest Beta (${response.headers["x-ratelimit-remaining"] + "/" + response.headers["x-ratelimit-limit"]}) | ${new Date().getHours() + ':' + new Date().getMinutes()}` 
+        : `AniSuggest Beta | ${new Date().getHours() + ':' + new Date().getMinutes()}`
+    return { text: footerString }
 }
