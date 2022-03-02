b76f10c4475a3e090dbc4b2725aa12e74fe9e03e
Sayykii <martin40lmg@gmail.com>
2022-03-02 21:25:47 +0200

    Added pagination to airing

--- Commands/airing.js
+++ Commands/airing.js
@@ -48,7 +48,6 @@ module.exports = new Command({
 
         const _day = new Date(Date.now() + diff);
         const day = new Date(Date.UTC(_day.getFullYear(), _day.getMonth(), _day.getDate()));
-        console.log(day);
 
         if (!hook) {
             //vars.query = args.slice(1).join(" ");
@@ -56,7 +55,6 @@ module.exports = new Command({
             nextDay.setHours(23, 59, 59, 999);
             vars.dateStart = Math.floor(day.getTime() / 1000);
             vars.nextDay = Math.floor(nextDay.getTime() / 1000);
-            console.log(nextDay);
         } else if (hook && hookdata?.title) vars.query = hookdata.title;
         else if (hook && hookdata?.id) vars.query = hookdata.id;
         else return message.channel.send({ embeds: [EmbedError(`AnimeCmd was hooked, yet there was no title or ID provided in hookdata.`, null, false)] });
@@ -84,11 +82,14 @@ module.exports = new Command({
                     for (i = 0, j = airingSchedules.length; i < j; i += chunk) {
                         fields.push(airingSchedules.slice(i, i + chunk));
                     }
+
+                    let pageList = [];
                     fields.forEach((fieldSet, index) => {
                         let embed = new Discord.MessageEmbed();
                         embed.setTitle(`Airing on ${day.toDateString()}`);
                         embed.setColor(0x00ff00);
                         embed.setFooter(Footer(response));
+
                         fieldSet.forEach((field) => {
                             const { media, episode, airingAt } = field;
                             const { title } = media;
@@ -98,11 +99,37 @@ module.exports = new Command({
                                 value: ` - EP - ${episode} ${new Date(airingAt * 1000) > new Date() ? `Going to air <t:${airingAt}:R>` : `Aired <t:${airingAt}:R>`}`,
                                 inline: false,
                             });
-
                         });
-                        message.channel.send({ embeds: [embed] });
+                        pageList.push(embed);
                     });
+                    const buttonList = [
+                        new Discord.MessageButton().setCustomId("firstbtn").setLabel("First page").setStyle("DANGER"),
+                        new Discord.MessageButton().setCustomId("previousbtn").setLabel("Previous").setStyle("SUCCESS"),
+                        new Discord.MessageButton().setCustomId("nextbtn").setLabel("Next").setStyle("SUCCESS"),
+                        new Discord.MessageButton().setCustomId("lastbtn").setLabel("Last Page").setStyle("DANGER"),
+                    ];
 
+                    pagination({
+                        message, // Required
+                        pageList, // Required
+                        buttonList,
+                        autoButton: true, // optional - if you do not want custom buttons remove the buttonList parameter
+                        // and replace it will autoButtons: true which will create buttons depending on
+                        // how many pages there are
+                        autoDelButton: true, // Optional - if you are using autoButton and would like delete buttons this
+                        // parameter adds delete buttons to the buttonList
+
+                        timeout: 20000, // Optional - if not provided it will default to 12000ms
+
+                        replyMessage: true, // Optional - An option to reply to the target message if you do not want
+                        // this option remove it from the function call
+
+                        autoDelete: true, // Optional - An option to have the pagination delete it's self when the timeout ends
+                        // if you do not want this option remove it from the function call
+
+                        authorIndependent: true, // Optional - An option to set pagination buttons only usable by the author
+                        // if you do not want this option remove it from the function call
+                    });
                     if (hookdata?.image) {
                         firstPage.setImage(hookdata.image);
                     }
