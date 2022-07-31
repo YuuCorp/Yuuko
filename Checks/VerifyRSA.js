ef02a666124c29abc7be67de49848677fbf37d4b
crackheadakira <lasn.mine@gmail.com>
2022-07-31 15:21:03 +0300

    Add extra check for RSA

--- Checks/VerifyRSA.js
+++ Checks/VerifyRSA.js
@@ -1,6 +1,9 @@
 const Check = require("#Structures/Check.js");
 const fs = require("fs");
 const path = require("path");
+const NodeRSA = require('node-rsa');
+const encryptor = new NodeRSA(fs.readFileSync(path.join(__dirname, '../RSA/id_rsa.pub').toString()));
+const decryptor = new NodeRSA(fs.readFileSync(path.join(__dirname, '../RSA/id_rsa').toString()));
 
 const rsaPublicCheck = new Check({
     name: "RSA Public Key Check",
@@ -24,4 +27,15 @@ const rsaPrivateCheck = new Check({
     }
 })
 
-module.exports = [rsaPublicCheck, rsaPrivateCheck]
+const rsaCryptionCheck = new Check({
+    name: "RSA Encrypiton & Decryption Check",
+    description: "Ensure that the RSA key's are valid by encryping & decrypting them seperately using public & private keys.",
+    optional: false,
+    run: () => {
+        if (decryptor.decrypt(encryptor.encrypt('hello', 'base64'), 'utf8') != "hello") {
+            throw new Error("Public & Private RSA keys don't match.")
+        }
+    }
+})
+
+module.exports = [rsaPublicCheck, rsaPrivateCheck, rsaCryptionCheck]
