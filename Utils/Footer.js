78ee555091dea6e5cb58c2d26d4f4820f5b68560
TibixDev <fuloptibi03@gmail.com>
2022-09-26 00:13:25 +0300

    [⌚] Footer time formatting fix

--- Utils/Footer.js
+++ Utils/Footer.js
@@ -1,11 +1,22 @@
 /**
+ * Get a formatted HH:MM time
+ */
+function formattedTime() {
+    let hours = new Date().getHours();
+    let mins = new Date().getMinutes();
+    hours = hours < 10 ? `0${hours}` : hours;
+    mins = mins < 10 ? `0${mins}` : mins;
+    return `${hours}:${mins}`;
+}
+
+/**
  * Returns the footer string.
  * @param [headers=null] Optional HTTP headers to get the ratelimit values.
  * @returns {Object} The footer object.
  */
 module.exports = (headers = null) => {
     const footerString = headers
-        ? `Yuuko Beta (${headers["x-ratelimit-remaining"] + "/" + headers["x-ratelimit-limit"]}) | ${new Date().getHours() + ':' + new Date().getMinutes()}`
-        : `Yuuko Beta | ${new Date().getHours() + ':' + new Date().getMinutes()}`
+        ? `Yuuko Beta (${headers["x-ratelimit-remaining"] + "/" + headers["x-ratelimit-limit"]}) | ${formattedTime()}`
+        : `Yuuko Beta | ${formattedTime()}`
     return { text: footerString }
 }
