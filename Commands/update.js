0bad583ed39e08de7453f903a91cd4af0249a146
crackheadakira <lasn.mine@gmail.com>
2022-10-09 00:07:25 +0200

    Add error handler to update command

--- Commands/update.js
+++ Commands/update.js
@@ -18,59 +18,63 @@ module.exports = new Command({
         .setDescription(description),
 
     async run(interaction, args, run) {
+        try {
 
-        if (JSON.parse(process.env.TRUSTED_USERS).includes(interaction.user.id)/* && process.env.NODE_ENV === "production"*/) {
-            let updateMessage = await interaction.reply("Updating...");
-            const editMessage = async content => {
-                await interaction.editReply("```sh\n" + content + "```");
+            if (JSON.parse(process.env.TRUSTED_USERS).includes(interaction.user.id)/* && process.env.NODE_ENV === "production"*/) {
+                let updateMessage = await interaction.reply("Updating...");
+                const editMessage = async content => {
+                    await interaction.editReply("```sh\n" + content + "```");
+                }
+
+                const update = spawn('sh', ['update.sh']);
+                let updateLogs = ""
+
+                let rateLimitCurrent = 0;
+                let rateLimitMax = 4;
+                let updateInterval = setInterval(() => {
+                    if (rateLimitCurrent < rateLimitMax) {
+                        console.log(`Sending update logs ${rateLimitCurrent}/${rateLimitMax}`);
+                        editMessage(updateLogs);
+                        rateLimitCurrent++;
+                    } else {
+                        clearInterval(updateInterval);
+                    }
+                }, 500);
+                update.stdout.on('data', data => {
+                    console.log(data.toString());
+                    updateLogs += data;
+                });
+                update.stderr.on('data', data => {
+                    console.log(data.toString());
+                    updateLogs += `STDERR: ${data}`;
+                });
+                update.on('close', async code => {
+                    console.log(`Procedures completed with code ${code}, restarting...`);
+                    updateLogs += `Procedures completed with code ${code}, restarting...  `;
+                    if (updateInterval) clearInterval(updateInterval);
+                    await editMessage(updateLogs);
+                    //await interaction.deferEdit();
+
+                    // Make a temporary file that stores the ID of the message sent, and the channel ID it was sent in
+                    // This is so that the bot can react to the message after restarting
+                    if (!fs.existsSync(path.join(__dirname, "../Local"))) {
+                        fs.mkdirSync(path.join(__dirname, "../Local"));
+                    }
+
+                    let reply = await interaction.fetchReply();
+                    const tempFile = path.join(__dirname, "../Local/updatemsg.json");
+                    console.log(reply)
+                    //const tempFileData = {
+                    //    messageID: reply.id,
+                    //    channelID: reply.channelId
+                    //}
+                    //fs.writeFileSync(tempFile, JSON.stringify(tempFileData));
+                    //execSync('git rev-parse --short HEAD > commit.hash', { encoding: 'utf-8' });
+                    execSync('pm2 restart "Yuuko Production"', { encoding: 'utf-8' });
+                });
             }
-
-            const update = spawn('sh', ['update.sh']);
-            let updateLogs = ""
-
-            let rateLimitCurrent = 0;
-            let rateLimitMax = 4;
-            let updateInterval = setInterval(() => {
-                if (rateLimitCurrent < rateLimitMax) {
-                    console.log(`Sending update logs ${rateLimitCurrent}/${rateLimitMax}`);
-                    editMessage(updateLogs);
-                    rateLimitCurrent++;
-                } else {
-                    clearInterval(updateInterval);
-                }
-            }, 500);
-            update.stdout.on('data', data => {
-                console.log(data.toString());
-                updateLogs += data;
-            });
-            update.stderr.on('data', data => {
-                console.log(data.toString());
-                updateLogs += `STDERR: ${data}`;
-            });
-            update.on('close', async code => {
-                console.log(`Procedures completed with code ${code}, restarting...`);
-                updateLogs += `Procedures completed with code ${code}, restarting...  `;
-                if (updateInterval) clearInterval(updateInterval);
-                await editMessage(updateLogs);
-                //await interaction.deferEdit();
-
-                // Make a temporary file that stores the ID of the message sent, and the channel ID it was sent in
-                // This is so that the bot can react to the message after restarting
-                if (!fs.existsSync(path.join(__dirname, "../Local"))) {
-                    fs.mkdirSync(path.join(__dirname, "../Local"));
-                }
-
-                let reply = await interaction.fetchReply();
-                const tempFile = path.join(__dirname, "../Local/updatemsg.json");
-                console.log(reply)
-                //const tempFileData = {
-                //    messageID: reply.id,
-                //    channelID: reply.channelId
-                //}
-                //fs.writeFileSync(tempFile, JSON.stringify(tempFileData));
-                //execSync('git rev-parse --short HEAD > commit.hash', { encoding: 'utf-8' });
-                execSync('pm2 restart "Yuuko Production"', { encoding: 'utf-8' });
-            });
+        } catch (error) {
+            console.log(error)
         }
     }
 });
