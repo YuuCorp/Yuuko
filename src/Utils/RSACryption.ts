b496410b8d43be8c980261b6eac3ff65aa8a985b
Morgan Tali <33316699+crackheadakira@users.noreply.github.com>
2023-11-14 17:18:43 +0100

    Merge dev to main (#24)
    * Change Log
    
    - Update RSACryption to use SubtleCrypto instead of NodeRSA for better performance
    - Update all the places where RSACryption is used to now await, since it is now async
    
    * Change Log
    
    fix(handleMedia): incorrect null checking for list entries
    refactor(decrypt): use cryptokey to calculate chunk size
    
    * Update RSACryption for better readability
    
    ---------
    
    Co-authored-by: sayykii <sayykiipersonal@gmail.com>
    Co-authored-by: Morgan Tali <crackheadakira@users.noreply.github.com>
    Co-authored-by: Sayykii <Sayykii@users.noreply.github.com>

--- src/Utils/RSACryption.ts
+++ src/Utils/RSACryption.ts
@@ -1,25 +1,100 @@
 import fs from "fs";
 import path from "path";
-import NodeRSA from "node-rsa";
 
+
+type RSAkey = CryptoKey & { algorithm: { modulusLength: number } };
 /**
  *
  * @param item The item to decrypt/encrypt.
  * @param encrypt An optional boolean to specify if you want to encrypt the item.
  * @example RSACryption("Hello", true) // Encrypts the string "Hello"
  */
-export function RSACryption(item: string, encrypt?: boolean): string {
-  if (encrypt) {
-    const publicRSA = path.join(__dirname, "..", "RSA", "id_rsa.pub");
-    if (!fs.existsSync(publicRSA)) throw new Error("Missing public RSA key.");
-
-    const encryptitem = new NodeRSA(fs.readFileSync(publicRSA, 'utf8'));
-    return encryptitem.encrypt(item, "base64");
-  }
-
-  const privateRSA = path.join(__dirname, "..", "RSA", "id_rsa");
-  if (!fs.existsSync(privateRSA)) throw new Error("Missing Private RSA key.");
+export async function RSACryption(item: string, encrypt?: boolean): Promise<string> {
+  const enc = new TextEncoder();
+  const dec = new TextDecoder();
   
-  const decryptitem = new NodeRSA(fs.readFileSync(privateRSA, 'utf8'));
-  return decryptitem.decrypt(item, "utf8");
+  if (encrypt) {
+    const itemBuffer = enc.encode(item);
+    const _publicRSA = path.join(__dirname, "..", "RSA", "id_rsa.pub")
+    if (!fs.existsSync(_publicRSA)) throw new Error("Missing public RSA key.");
+
+    let publicRSA = fs.readFileSync(_publicRSA, "utf8")
+    publicRSA = publicRSA.replace('-----BEGIN PUBLIC KEY-----', '').replace('-----END PUBLIC KEY-----', '').replaceAll('\n', '');
+    const publicRSAData = str2ab(atob(publicRSA));
+    const cryptoKey = await globalThis.crypto.subtle.importKey('spki', publicRSAData, { name: 'RSA-OAEP', hash: 'SHA-256' }, false, ['encrypt']) as RSAkey;
+    // https://crypto.stackexchange.com/questions/42097/what-is-the-maximum-size-of-the-plaintext-message-for-rsa-oaep
+    // ^ According to this comment we can calculate the chunk size with the formula ( ( modulusLength / 8 ) - ( 2 * hLenBits / 8 ) - 2 )
+    // hLenBits is the length of the hash function output in bits, which is 256 for SHA-256
+    const chunkSize = cryptoKey.algorithm.modulusLength / 8 - 2 * 256 / 8 - 2;
+
+    if(itemBuffer.byteLength > chunkSize ) {
+      const chunks = [];
+      for (let i = 0; i < itemBuffer.byteLength; i += chunkSize) {
+        chunks.push(itemBuffer.slice(i, i + chunkSize));
+      }
+      const encryptedChunks: ArrayBuffer[] = [];
+      for (const chunk of chunks) {
+        encryptedChunks.push(await globalThis.crypto.subtle.encrypt('RSA-OAEP', cryptoKey, chunk));
+      }
+      
+      const encryptedItem = new Uint8Array(encryptedChunks.reduce((acc, chunk) => acc + chunk.byteLength, 0));
+      let offset = 0;
+      for (const chunk of encryptedChunks) {
+        encryptedItem.set(new Uint8Array(chunk), offset);
+        offset += chunk.byteLength;
+      }
+      return abtob64(encryptedItem);
+    }
+    const encryptItem = await globalThis.crypto.subtle.encrypt('RSA-OAEP', cryptoKey, itemBuffer);
+    return abtob64(encryptItem);
+  }
+  const _privateRSA = path.join(__dirname, "..", "RSA", "id_rsa") 
+  if (!fs.existsSync(_privateRSA)) throw new Error("Missing private RSA key.");
+
+  let privateRSA = fs.readFileSync(_privateRSA, "utf8");
+  privateRSA = privateRSA.replace('-----BEGIN PRIVATE KEY-----', '').replace('-----END PRIVATE KEY-----', '').replaceAll('\n', '');
+  const privateRSAData = str2ab(atob(privateRSA));
+  const cryptoKey = await globalThis.crypto.subtle.importKey('pkcs8', privateRSAData, { name: 'RSA-OAEP', hash: 'SHA-256' }, false, ['decrypt']) as RSAkey;
+
+  const itemBuffer = str2ab(atob(item));
+  const chunkSize = cryptoKey.algorithm.modulusLength / 8;
+
+  if(itemBuffer.byteLength > chunkSize) {
+    const chunks = [];
+    for (let i = 0; i < itemBuffer.byteLength; i += chunkSize) {
+      chunks.push(itemBuffer.slice(i, i + chunkSize));
+    }
+    const decryptedChunks: ArrayBuffer[] = [];
+    for (const chunk of chunks) {
+      decryptedChunks.push(await globalThis.crypto.subtle.decrypt('RSA-OAEP', cryptoKey, chunk));
+    }
+    
+    const decryptedItem = new Uint8Array(decryptedChunks.reduce((acc, chunk) => acc + chunk.byteLength, 0));
+    let offset = 0;
+    for (const chunk of decryptedChunks) {
+      decryptedItem.set(new Uint8Array(chunk), offset);
+      offset += chunk.byteLength;
+    }
+    return dec.decode(decryptedItem);
+  }
+  const decryptItem = await globalThis.crypto.subtle.decrypt('RSA-OAEP', cryptoKey, itemBuffer);
+  return dec.decode(decryptItem);
+}
+
+function str2ab(str: string) {
+  const buf = new ArrayBuffer(str.length);
+  const bufView = new Uint8Array(buf);
+  for (let i = 0, strLen = str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
+}
+
+function abtob64(buffer: Uint8Array | ArrayBuffer) {
+  const binary = [];
+  const bytes = new Uint8Array(buffer);
+  for (let i = 0; i < bytes.byteLength; i++)
+    binary.push(String.fromCharCode(bytes[i]!));
+
+  return btoa(binary.join(''));
 }
