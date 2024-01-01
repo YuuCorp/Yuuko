3b35544fad327f89a76b4a4bca99357718f482cc
Morgan Tali <33316699+crackheadakira@users.noreply.github.com>
2024-01-01 15:31:15 +0100

    Create a cronjob to reset logs on the first of the month (#31)
    * Fix logs.txt not being correctly made on first run
    
    * Fix missing better-sqlite3 and add drizzzle folder to gitignore
    
    * Incorrectly added drizzle to gitignore
    
    * Cronjob gets made when running start.sh

--- src/Utils/Logging.ts
+++ src/Utils/Logging.ts
@@ -7,14 +7,7 @@ export function Logging(command: Command, interaction: Interaction) {
   try {
     const logPath = path.join(__dirname, '..', 'Logging/logs.txt')
     const currentDate = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
-    if (!fs.existsSync(path.join(__dirname, '..', 'Logging'))) {
-      fs.mkdirSync(path.join(__dirname, '..', 'Logging'))
-      // check if logPath exists
-      if (!fs.existsSync(logPath)) {
-        // create logPath 
-        fs.writeFileSync(logPath, 'Initiating log!')
-      }
-    }
+
     if (!interaction.isCommand()) return
     const subcommand = (interaction.options as CommandInteractionOptionResolver).getSubcommand(false)
     const log = `${fs.readFileSync(logPath, 'utf8').toString()}\n` + `${currentDate}: ${interaction.user.username}#${interaction.user.discriminator} ran command: ${command.name}${subcommand ? ` ${subcommand}` : ''}`
