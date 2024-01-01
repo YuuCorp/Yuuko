8e9aca6c7984982262c302037b2f3cbc96cfe4d9
Morgan Tali <lasn.mine@gmail.com>
2024-01-01 15:50:36 +0100

    Add absolute log path to cron

--- start.sh
+++ start.sh
@@ -4,14 +4,16 @@ bun i
 git rev-parse --short HEAD > commit.hash
 
 # Cron job to reset logs every month
+LOG_DIRECTORY=$(readlink -f src/Logging/logs.txt)
 crontab -l > mycron
 
 # TO prevent duplicate cron jobs from being added we remove all lines that contain the path to the logs.txt file
 # so it won't disrupt the user's other cron jobs
-grep -v "> src/Logging/logs.txt" mycron > tmpfile
+grep -v "> ${LOG_DIRECTORY}" mycron > tmpfile
 mv tmpfile mycron
 
-echo "0 0 1 * * echo date "+%Y-%m-%d %T: Resetting logs" > src/Logging/logs.txt" >> mycron
+echo "0 0 1 * * echo date "+%Y-%m-%d %T: Resetting logs" > ${LOG_DIRECTORY}" >> mycron
 crontab mycron
+rm mycron
 
 pm2 start ecosystem.config.cjs --env production
