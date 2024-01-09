a1492f1ee0a770eca8fb689fbc5b0befadcee4ed
Morgan Tali <lasn.mine@gmail.com>
2024-01-09 17:15:43 +0100

    Generate a RSA key pair on first startup

--- src/app.ts
+++ src/app.ts
@@ -33,4 +33,34 @@ async function start(token: string | undefined) {
 
   process.env.UPTIME = Date.now();
 }
+
+async function makeRSAPair() {
+  const RSAdirectory = path.join(__dirname, 'RSA');
+  if(fs.existsSync(RSAdirectory)) return;
+  const dec = new TextDecoder();
+
+  const keyPair = await globalThis.crypto.subtle.generateKey({
+    name: "RSA-OAEP",
+    modulusLength: 4096,
+    publicExponent: new Uint8Array([0x01, 0x00, 0x01]), // Value taken from https://developer.mozilla.org/en-US/docs/Web/API/RsaHashedKeyGenParams#publicexponent
+    hash: "SHA-256",
+  }, true, ['encrypt', 'decrypt'])
+
+  const publicKey = await globalThis.crypto.subtle.exportKey('spki', keyPair.publicKey);
+  const privateKey = await globalThis.crypto.subtle.exportKey('pkcs8', keyPair.privateKey)
+  
+  const exportedPublicKey = '-----BEGIN PUBLIC KEY-----\n' + 
+          btoa(String.fromCharCode.apply(null, [...new Uint8Array(publicKey)])).replace(/.{64}/g, '$&\n') + '\n-----END PUBLIC KEY-----';
+  const exportedPrivateKey = '-----BEGIN PRIVATE KEY-----\n' + // Inserts a newline every 64 characters
+          btoa(String.fromCharCode.apply(null, [...new Uint8Array(privateKey)])).replace(/.{64}/g, '$&\n') + '\n-----END PRIVATE KEY-----';
+
+  fs.mkdirSync(RSAdirectory);
+
+  fs.writeFileSync(path.join(RSAdirectory, 'id_rsa'), exportedPrivateKey);
+  fs.writeFileSync(path.join(RSAdirectory, 'id_rsa.pub'), exportedPublicKey);
+
+  client.log("Successfully generated the RSA key pair!")
+}
+
+makeRSAPair();
 start(process.env.TOKEN);
