fbd5043662ebe754e71cd3e7367a77461d303167
crackheadakira <lasn.mine@gmail.com>
2024-03-03 18:19:31 +0100

    Move the logic for updating bot stats to a util

--- src/Utils/getStats.ts
+++
@@ -1,8 +0,0 @@
-import { stat, statTables } from "../Database/db";
-
-export async function getStats() {
-  const stats = (await stat.select().from(statTables.BotStats))[0];
-  console.log(stats);
-  if (!stats) return { servers: 0, members: 0, registered: 0 };
-  return stats;
-}
