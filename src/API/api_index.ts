e1e1c290077f0794ad5d6d97d86f449122c8e36e
crackheadakira <lasn.mine@gmail.com>
2024-03-03 18:30:17 +0100

    Update ESLint settings

--- src/API/api_index.ts
+++ src/API/api_index.ts
@@ -1,11 +1,11 @@
-import { Elysia } from "elysia";
-import { api } from "./controllers/*";
-import type { Client } from "#Structures/index.js";
+import { Elysia } from 'elysia'
+import { api } from './controllers/*'
+import type { Client } from '#Structures/index.js'
 
 export async function start_API(client: Client) {
-  const port = process.env.PORT;
+  const port = process.env.PORT
 
-  new Elysia().use(api).listen(port);
+  new Elysia().use(api).listen(port)
 
-  client.log(`API is open on port ${port}`);
+  client.log(`API is open on port ${port}`)
 }
