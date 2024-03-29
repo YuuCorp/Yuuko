19ad5b62ab107124ad79e3b89ffa7f273e4dc24f
Morgan Tali <33316699+crackheadakira@users.noreply.github.com>
2024-03-30 00:21:36 +0100

    Add CORS (#38)

--- src/API/api_index.ts
+++ src/API/api_index.ts
@@ -1,3 +1,4 @@
+import { cors } from '@elysiajs/cors'
 import { Elysia } from 'elysia'
 import { api } from './controllers/*'
 import type { Client } from '#Structures/index.js'
@@ -5,7 +6,9 @@ import type { Client } from '#Structures/index.js'
 export async function start_API(client: Client) {
   const port = process.env.PORT
 
-  new Elysia().use(api).listen(port)
+  new Elysia().use(cors({
+    origin: /.*\.yuuko\.dev$/,
+  })).use(api).listen(port)
 
   client.log(`API is open on port ${port}`)
 }
