c42dfa62d267bfb89974c9f9f3c57fc211c73bad
Morgan Tali <33316699+crackheadakira@users.noreply.github.com>
2024-04-19 01:08:18 +0200

    Change database folder structure for docker (#48)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "src/database/schema.ts",
-  out: "./src/databaseMigration/drizzle",
-  driver: "libsql",
-  dbCredentials: {
-    url: "file:./src/database/db.sqlite",
-  },
+    schema: "src/database/schema.ts",
+    out: "./src/databaseMigration/drizzle",
+    driver: "libsql",
+    dbCredentials: {
+        url: "file:./src/database/sqlite/db.sqlite",
+    },
 } satisfies Config;
