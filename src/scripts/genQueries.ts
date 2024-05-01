11237b8e0bfddbfbfebf2b5292b77eff0d91461e
crackheadakira <lasn.mine@gmail.com>
2024-05-01 16:25:05 +0200

    Update query

--- src/scripts/genQueries.ts
+++ src/scripts/genQueries.ts
@@ -2,18 +2,18 @@ import fs from 'node:fs'
 import path from 'node:path';
 
 (async () => {
-  const fileNames = fs.readdirSync('./src/GraphQL').filter(x => x.endsWith('.gql'))
+  const fileNames = fs.readdirSync('./src/graphQL').filter(x => x.endsWith('.gql'))
 
   const out: Record<string, string> = {};
 
   fileNames.forEach((file) => {
-    const queryStr = fs.readFileSync(`./src/GraphQL/${file}`, 'utf8')
+    const queryStr = fs.readFileSync(`./src/graphQL/${file}`, 'utf8')
     out[file.split('.')[0]!] = queryStr
   })
 
-  await Bun.write('./src/GraphQL/types/queries.ts', `export default ${JSON.stringify(out, null, 2)} as const`)
+  await Bun.write('./src/graphQL/types/queries.ts', `export default ${JSON.stringify(out, null, 2)} as const`)
 
-  const fullPath = path.join(import.meta.dir, '../GraphQL/types/queries.ts')
+  const fullPath = path.join(import.meta.dir, '../graphQL/types/queries.ts')
 
   console.log(`Finished generating queries. ${fullPath}`)
 })()
