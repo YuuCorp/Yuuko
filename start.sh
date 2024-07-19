1eefacf1b02288195c0ab2241f7f73c3a4df085b
Morgan Tali <33316699+crackheadakira@users.noreply.github.com>
2024-07-19 21:20:53 +0200

    Hardcode less items in start.sh for cronjob (#61)

--- start.sh
+++ start.sh
@@ -4,16 +4,15 @@ bun i --production
 git rev-parse --short HEAD > commit.hash
 
 # Cron job to reset logs every month
-LOG_DIRECTORY=$(readlink -f src/Logging/logs.txt)
+LOG_DIRECTORY=$(readlink -f src/Logging/logs.json)
 crontab -l > mycron
 
-# TO prevent duplicate cron jobs from being added we remove all lines that contain the path to the logs.txt file
+# TO prevent duplicate cron jobs f  rom being added  we remove all lines that contain the path to the logs.json file
 # so it won't disrupt the user's other cron jobs
 grep -v "> ${LOG_DIRECTORY}" mycron > tmpfile
 mv tmpfile mycron
-
-echo "0 0 1 * * echo date "+%Y-%m-%d %T: Resetting logs" > ${LOG_DIRECTORY}" >> mycron
+echo "0 0 1 * * echo '[{ \"date\": \"\$(date +\%Y-\%m-\%d \%T)\", \"user\": \"SYSTEM_LOGGER\", \"info\": \"Reset log!\" }]' > ${LOG_DIRECTORY}" >> mycron
 crontab mycron
-rm mycron
+rm mycron           
 
 pm2 start ecosystem.config.cjs --env production
