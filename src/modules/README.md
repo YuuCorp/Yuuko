54bdc8f467f81a0af252ee90eb2fc4a6849f282c
crackheadakira <lasn.mine@gmail.com>
2025-04-21 16:55:53 +0200

    Include compiled library

---
+++ src/modules/README.md
@@ -0,0 +1,3 @@
+# Modules
+
+Here we'll have our modules that we can use in Bun via FFI, we will include the compiled libraries, but it is highly recommended to build them yourself via `bun run module:build`
--- src/modules/build.sh
+++ src/modules/build.sh
@@ -5,7 +5,21 @@ set -e
 SRC_FILE="./src/lib.rs"
 MODULE_DIR="./src/modules"
 OUT_DIR="./compiled"
-OUT_FILE="libmodules.so"
+OUT_FILE="libmodules"
+
+# Determine platform to get library suffix
+case "$(uname -s)" in
+    Darwin*)  # macOS
+        OUT_FILE="$OUT_FILE.dylib"
+        ;;
+    Linux*)   # Linux
+        OUT_FILE="$OUT_FILE.so"
+        ;;
+    CYGWIN*|MINGW32*|MSYS*|MINGW*) # Windows (MinGW or Cygwin)
+        OUT_FILE="$OUT_FILE.dll"
+        ;;
+    *) echo "Unknown platform"; exit 1 ;;
+esac
 
 (
     cd "$MODULE_DIR"
@@ -13,9 +27,9 @@ OUT_FILE="libmodules.so"
     mkdir -p "$OUT_DIR"
 
     echo "🛠️  Building $SRC_FILE → $OUT_DIR/$OUT_FILE"
-    cargo build --release --target-dir="$OUT_DIR"
+    cargo build --release
 
-    mv "$OUT_DIR/release/libmodules.so" "$OUT_DIR/$OUT_FILE"
+    mv "./target/release/libmodules.so" "$OUT_DIR/$OUT_FILE"
 
     echo "✅ Done: $OUT_DIR/$OUT_FILE"
 )
