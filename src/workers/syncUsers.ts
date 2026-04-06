e218c1e92a2c1d4ba233ab631e61c5076005d03f
Morgan Tali <33316699+crackheadakira@users.noreply.github.com>
2026-04-07 01:01:09 +0200

    fix: do first sync on boot-up (#81)

--- src/workers/syncUsers.ts
+++ src/workers/syncUsers.ts
@@ -12,65 +12,43 @@ export async function syncAnilistUsers(data: SyncUsers) {
     const timeOut = Math.ceil(60 * 1000 / usersPerMinute);
 
     const total = anilistUsers.length;
-    let i = 1;
+
+    client.log(`Preparing to sync a total of ${anilistUsers.length} users.`, "verbose");
 
     const date = Math.floor(Date.now() / 1000);
-    for (const user of anilistUsers) {
-        try {
-            // GetUserList is a more optimized query for syncing lists compared to GetMediaCollection
-            // as we don't care about the media, only user entries
-            const start = performance.now();
 
-            // If the user's token is expired, instantly delete, saves on requests to AniList
+    for (let i = 0; i < total; i++) {
+        const user = anilistUsers[i]!;
+        try {
+            const start = performance.now();
             const { payload } = decodeJWT(user.anilistToken);
             if (date >= payload.exp) {
-                i++
                 await deleteUser(user.anilistId);
                 continue;
             }
-
-            const { data: animeData } = await graphQLRequest("GetUserList", {
-                userId: user.anilistId,
-                type: MediaType.Anime,
-            }, user.anilistToken);
-
+            const { data: animeData } = await graphQLRequest("GetUserList", { userId: user.anilistId, type: MediaType.Anime }, user.anilistToken);
             if (animeData) await handleSyncing({ media: animeData }, user.anilistId, MediaType.Anime);
-
-            const { data: mangaData } = await graphQLRequest("GetUserList", {
-                userId: user.anilistId,
-                type: MediaType.Manga,
-            }, user.anilistToken);
-
+            const { data: mangaData } = await graphQLRequest("GetUserList", { userId: user.anilistId, type: MediaType.Manga }, user.anilistToken);
             if (mangaData) await handleSyncing({ media: mangaData }, user.anilistId, MediaType.Manga);
-
-            client.log(`Synced user ${user.anilistId} (${i} / ${anilistUsers.length})`, "verbose");
-
+            client.log(`Synced user ${user.anilistId} (${i + 1} / ${total})`, "verbose");
             const localTimeout = Math.max(0, Math.floor(timeOut - (performance.now() - start)));
-            if (i <= total && localTimeout > 0) {
+            if (i < total - 1 && localTimeout > 0) {
                 await new Promise((resolve) => setTimeout(resolve, localTimeout));
             }
-            i++;
-
         } catch (e) {
             console.error(e);
-
-            if (e instanceof YuukoError) {
-                if (e.message.toLowerCase().includes("invalid token")) {
-                    await deleteUser(user.anilistId);
-                }
-            };
-
-            if (i <= total) {
+            if (e instanceof YuukoError && e.message.toLowerCase().includes("invalid token")) {
+                await deleteUser(user.anilistId);
+            }
+            if (i < total - 1) {
                 await new Promise((resolve) => setTimeout(resolve, timeOut));
             }
-
-            i++;
         }
     }
 }
 
 async function deleteUser(anilistId: number) {
-    client.log(`User ${anilistId} has an invalid token, deleting from the DB...`, "debug");
+    client.log(`User ${anilistId} has an invalid token, deleting from the DB...`, "verbose");
     await db
         .delete(tables.anilistUser)
         .where(eq(tables.anilistUser.anilistId, anilistId));
